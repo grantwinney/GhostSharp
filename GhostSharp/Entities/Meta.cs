077a777e563c2f0659d725eb799cba0db9f24ff0
Grant Winney <grantwinney@gmail.com>
2018-02-11 14:18:36 -0500

    added comments to the meta class

--- GhostSharp/Entities/Meta.cs
+++ GhostSharp/Entities/Meta.cs
@@ -1,11 +1,30 @@
-﻿using System;
-namespace GhostSharp.Entities
+﻿namespace GhostSharp.Entities
 {
-    public class Meta
+    /// <summary>
+    /// Holds information about the request, such as metadata about the 'page' of data that was requested.
+    /// </summary>
+    public class Meta   
     {
         public Pagination Pagination { get; set; }
     }
 
+    /// <summary>
+    /// Represents the current 'page' of data you've retrieved, controlled by the Limit and Page values.
+    /// </summary>
+    /// <remarks>
+    /// When fetching multiple resources from an endpoint, e.g. performing a 'browse' request
+    /// the number of items returned will be limited to 15 by default.
+    /// 
+    /// Ghost's API currently uses page-based pagination, which is controlled via the two
+    /// parameters Limit and Page. The API returns page 1 by default, and requesting page 2
+    /// will send the next 15 items. Alternatively you can make a request for more or less items,
+    /// so that you can get the exact data that you need, by changing the limit.
+    /// 
+    /// When browsing the posts, users and tags endpoints you will retrieve an array of your
+    /// queried resource and a meta object that includes a pagination object.
+    /// 
+    /// Source: https://api.ghost.org/docs/pagination
+    /// </remarks>
     public class Pagination
     {
         /// <summary>
