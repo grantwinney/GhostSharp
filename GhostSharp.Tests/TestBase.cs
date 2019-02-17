567fa407bc0975099fb43132aaa4d7a32725b28b
Grant Winney <1354059+grantwinney@users.noreply.github.com>
2019-02-17 15:29:31 -0500

    delete test settings (that api key no longer exists)

--- GhostSharp.Tests/TestBase.cs
+++ GhostSharp.Tests/TestBase.cs
@@ -10,13 +10,13 @@ namespace GhostSharpTests
         public static string Host = "https://grantwinney.com";
         public static string SiteTitle = "Grant Winney";
 
-        public static string ValidApiKey = "67674698fdcb50d7f7896981d0";
-        public static string ValidPostId = "5c60e815afe8720651f1834d";
-        public static string ValidPostSlug = "using-the-ip-geolocation-api-to-find-info-about-an-ip-address";
+        public static string ValidApiKey = "";
+        public static string ValidPostId = "";
+        public static string ValidPostSlug = "";
         public static string ValidAuthorId = "1";
-        public static string ValidAuthorSlug = "grant";
-        public static string ValidTagId = "5a32a221292eb46fca0fbe38";
-        public static string ValidTagSlug = "15-apis-in-15-days";
+        public static string ValidAuthorSlug = "";
+        public static string ValidTagId = "";
+        public static string ValidTagSlug = "";
 
         public static string InvalidApiKey = "{1D356678-846C-4C1F-97B3-3B87C008A2B2}";
         public static string InvalidPostId = "{1D356678-846C-4C1F-97B3-3B87C008A2B2}";
