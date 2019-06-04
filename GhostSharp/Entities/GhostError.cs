f143dd4ae91fdf21e1508629872e75503c326384
Grant Winney <1354059+grantwinney@users.noreply.github.com>
2019-06-04 16:15:36 -0400

    split classes into separate files

---
+++ GhostSharp/Entities/GhostError.cs
@@ -0,0 +1,14 @@
+﻿namespace GhostSharp.Entities
+{
+    /// <summary>
+    /// Represents a single error returned by the Ghost API.
+    /// </summary>
+    public class GhostError
+    {
+        public string Message { get; set; }
+        public string Context { get; set; }
+        public string ErrorType { get; set; }
+
+        public override string ToString() => $"{Message}";
+    }
+}
